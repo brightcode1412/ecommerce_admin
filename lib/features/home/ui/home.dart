979e708c1b94ee656b00146fe2d59e6994924e4b
MohamedMarzouk5 <mohamed.mz540@gmail.com>
2024-08-03 16:47:06 +0300

    home

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -4,12 +4,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class homeScreen extends StatelessWidget {
-  const homeScreen({super.key});
+  homeScreen({super.key});
+  String? _selectedCategory;
+  final List<String> _categories = ['Electronics', 'Books', 'Clothing', 'Home'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Upload Images')),
+      appBar: AppBar(title: const Text('Upload product')),
       body: BlocBuilder<ImageCubit, ImageState>(
         builder: (context, state) {
           return Column(
@@ -27,17 +29,61 @@ class homeScreen extends StatelessWidget {
                   onPressed: () => context.read<ImageCubit>().selectFiles(),
                   child: const Text('Select Images'),
                 ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: state.uploadedUrls.length,
-                  itemBuilder: (context, index) {
-                    return Image.network(
-                      state.uploadedUrls[index],
-                      width: 100,
-                      height: 100,
-                    );
-                  },
+
+              // ListView.builder(
+              //   scrollDirection: Axis.horizontal,
+              //   itemCount: state.uploadedUrls.length,
+              //   itemBuilder: (context, index) {
+              //     return Image.network(
+              //       state.uploadedUrls[index],
+              //       width: 100,
+              //       height: 100,
+              //     );
+              //   },
+              // ),
+              // drop down to select catogry name
+              Row(
+                children: [
+                  ...state.images.asMap().entries.map((e) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Stack(
+                          children: [
+                            Image.memory(
+                              e.value,
+                              width: 100,
+                              height: 100,
+                            ),
+                            Positioned(
+                              right: 0,
+                              top: 0,
+                              child: IconButton(
+                                  icon: const Icon(Icons.close),
+                                  onPressed: () {}
+                                  // context.read<ImageCubit>().removeImage(e.key),
+                                  ),
+                            ),
+                          ],
+                        ),
+                      )),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+
+              DropdownButton<String>(
+                value: _selectedCategory,
+                hint: const Text(
+                  'Select Category',
+                  style: TextStyle(color: Colors.black),
                 ),
+                items: _categories.map((String category) {
+                  return DropdownMenuItem<String>(
+                    value: category,
+                    child: Text(category),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {},
               ),
             ],
           );
