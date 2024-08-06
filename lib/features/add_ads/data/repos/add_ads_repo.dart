f47e82350091e38f106e0703f0608796798d53bf
MohamedMarzouk5 <mohamed.mz540@gmail.com>
2024-08-06 18:29:56 +0300

    ads

--- lib/features/add_ads/data/repos/add_ads_repo.dart
+++ lib/features/add_ads/data/repos/add_ads_repo.dart
@@ -1,9 +1,71 @@
+import 'dart:typed_data';
+
+import 'package:admin_e_commerce/core/services/firebase_error_handler.dart';
 import 'package:admin_e_commerce/core/services/firebase_services.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:logger/web.dart';
 
-class AddAdsRepo{
-
+class AddAdsRepo {
   final FirebaseServices _firebaseServices = FirebaseServices();
 
-  
+  final FirebaseStorage storage = FirebaseStorage.instance;
 
+  uploadAdsImage(List<String> imagesUrl) async {
+    try {
+      _firebaseServices.adsCollection.doc().update({
+        'image': imagesUrl,
+      });
+    } on FirebaseException catch (e) {
+      Logger().e('Error in uploadProduct: $e');
+      throw FirebaseNetworkException.errorHandler(e);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<Uint8List>> pickImages() async {
+    // Pick files
+    List<Uint8List> images = [];
+
+    final result = await FilePicker.platform
+        .pickFiles(allowMultiple: true, withData: true);
+    if (result != null) {
+      images = result.files.map((file) => file.bytes!).toList();
+    }
+    return images;
+  }
+
+  Future<List<String>> uploadImages(
+      List<Uint8List> images, Function(double) onProgress) async {
+    try {
+      List<String> downloadUrls = [];
+      int totalBytes = images.fold(0, (sum, image) => sum + image.length);
+      int bytesTransferred = 0;
+
+      for (var image in images) {
+        final ref = storage
+            .ref()
+            .child('uploads/${DateTime.now().millisecondsSinceEpoch}.png');
+        final uploadTask = ref.putData(image);
+
+        uploadTask.snapshotEvents.listen((event) {
+          if (event.state == TaskState.running) {
+            bytesTransferred += event.bytesTransferred;
+            final progress = (bytesTransferred / totalBytes) * 100;
+            onProgress(progress.toDouble());
+          }
+        });
+
+        await uploadTask;
+        final downloadUrl = await ref.getDownloadURL();
+        downloadUrls.add(downloadUrl);
+        print('Download URL: $downloadUrl');
+      }
+      return downloadUrls;
+    } catch (e) {
+      print('Error in uploadImages: $e');
+      return [];
+    }
+  }
 }
--- lib/features/add_ads/logic/cubit/add_ads_cubit.dart
+++ lib/features/add_ads/logic/cubit/add_ads_cubit.dart
@@ -1,6 +1,55 @@
+import 'dart:typed_data';
+
+import 'package:admin_e_commerce/core/services/failure.dart';
+import 'package:admin_e_commerce/features/add_ads/data/repos/add_ads_repo.dart';
 import 'package:admin_e_commerce/features/add_ads/logic/cubit/add_ads_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logger/web.dart';
 
 class AddAdsCubit extends Cubit<AddAdsState> {
   AddAdsCubit() : super(AddAdsInitial());
+
+  static AddAdsCubit get(context) => BlocProvider.of(context);
+  final AddAdsRepo _addAdsRepo = AddAdsRepo();
+
+  List<String> uploadedUrls = [];
+  List<Uint8List> images = [];
+  addAdds() async {
+    emit(UploadAdsLoading());
+    try {
+      if (uploadedUrls.isEmpty) {
+        emit(UploadAdsFailed('No images uploaded'));
+      } else {
+        await _addAdsRepo.uploadAdsImage(uploadedUrls);
+
+        emit(UploadAdsSuccess());
+      }
+    } on Failure catch (e) {
+      emit(UploadAdsFailed(e.toString()));
+    }
+  }
+
+  pickImages() async {
+    emit(ImageLoading(0));
+    images = await _addAdsRepo.pickImages();
+    if (images.isEmpty) {
+      emit(ImageUploadFailed('No images selected'));
+    } else {
+      try {
+        if (images.isNotEmpty) {
+          uploadedUrls =
+              await _addAdsRepo.uploadImages(images, (double progress) {
+            Logger().d('Progress: $progress');
+            emit(ImageLoading(progress));
+          });
+
+          emit(ImageUploadSuccess());
+        }
+      } on Failure catch (e) {
+        emit(ImageUploadFailed(e.toString()));
+      } catch (e) {
+        emit(ImageUploadFailed(e.toString()));
+      }
+    }
+  }
 }
